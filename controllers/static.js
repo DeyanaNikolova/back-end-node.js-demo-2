d949984814f30b42ce9306e6651d8574bd7b4503
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-17 19:05:54 +0000

    Add error handling to static

--- controllers/static.js
+++ controllers/static.js
@@ -3,15 +3,16 @@ const fs = require('fs');
 module.exports = (req, res) => {
     const fileName = req.url.slice(8);
 
-    try{
-        const file = fs.createReadStream(`./static/${fileName}`);
-        file.pipe(res);
-
-        // file.on('data', data => res.write(data));
-        // file.on('on', () => res.end());
-    } catch(err){
+    const file = fs.createReadStream(`./static/${fileName}`);
+    file.on('error', () => {
         res.statusCode = 404;
         res.write('Page is not found!');
         res.end();
-    }  
+    });
+
+    file.pipe(res);
+
+    // file.on('data', data => res.write(data));
+    // file.on('on', () => res.end());
+
 };

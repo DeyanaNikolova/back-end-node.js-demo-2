cefae1d1f9b0d8787205696619d6e3c9e03938a8
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-17 19:02:29 +0000

    Change in image routing

--- controllers/static.js
+++ controllers/static.js
@@ -1,8 +1,17 @@
 const fs = require('fs');
 
 module.exports = (req, res) => {
-    const file = fs.createReadStream('./static/cat.jpg');
-    file.pipe(res);
-    // file.on('data', data => res.write(data));
-    // file.on('on', () => res.end());
+    const fileName = req.url.slice(8);
+
+    try{
+        const file = fs.createReadStream(`./static/${fileName}`);
+        file.pipe(res);
+
+        // file.on('data', data => res.write(data));
+        // file.on('on', () => res.end());
+    } catch(err){
+        res.statusCode = 404;
+        res.write('Page is not found!');
+        res.end();
+    }  
 };

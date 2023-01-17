ae552ab216112927b43efeb8a2e2109bf609f43c
Deyana Nikolova <120827799+DeyanaNikolova@users.noreply.github.com>
2023-01-17 14:02:39 +0000

    Edit controllers

---
+++ controllers/catalogController.js
@@ -0,0 +1,20 @@
+const layout = require('../views/layout');
+const database = require('../util/database');
+
+const catalogPage = (items) => `
+<div>
+    <h1>Catalog</h1>
+    <form method="POST" action="/create">
+    <label>Name <input type="text" name="name"></label>
+    <label>S/N <input type="text" name="serial"></label>s
+    <input type="submit" value="Create Item">
+    </form>
+    <ul>
+        ${items.map(([id, i]) => `<li data-id="${id}">${i.name} - ${i.serial}<a href="/delete?id=${id}">Delete</a></li>`).join('')}
+    </ul>
+</div>`;
+
+module.exports = (req, res) => {
+    res.write(layout(catalogPage(Object.entries(database.database))));
+    res.end();
+};
---
+++ controllers/createController.js
@@ -0,0 +1,17 @@
+const formidable = require('formidable');
+const database = require('../util/database');
+
+module.exports = (req, res) => {
+    const form = new formidable.IncomingForm();
+
+    form.parse(req, (err, fields) => {
+        console.log('created item');
+
+        database.addItem(fields); 
+
+        res.writeHead(301, {
+            'Location': '/catalog'
+        });
+        res.end();
+    });  
+};
